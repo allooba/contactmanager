7b8dbc8220e33335e3ba67463f0743d8ac57f01d
Ali Abdelrhim <ali@Alis-MacBook-Pro.local>
2018-12-12 13:10:56 +0400

    Updates

---
+++ precache-manifest.fee730d766b576223c647e6addb9bd9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d32d13fb1107b878aac",
+    "url": "//githup.ip/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "5d32d13fb1107b878aac",
+    "url": "//githup.ip/contactmanager/static/js/main.5d32d13f.chunk.js"
+  },
+  {
+    "revision": "18420ff718b544224022",
+    "url": "//githup.ip/contactmanager/static/css/1.92e51617.chunk.css"
+  },
+  {
+    "revision": "18420ff718b544224022",
+    "url": "//githup.ip/contactmanager/static/js/1.18420ff7.chunk.js"
+  },
+  {
+    "revision": "ae0398e526bc360c4fbd",
+    "url": "//githup.ip/contactmanager/static/js/runtime~main.ae0398e5.js"
+  },
+  {
+    "revision": "e81df09645f443f69c0ed9c9b68d590d",
+    "url": "//githup.ip/contactmanager/index.html"
+  }
+];
